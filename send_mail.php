4037d4610ed45e1b744df1a1ed29cf91982ca1ef
WBElectric <bobkowojciech@gmail.com>
2025-08-20 13:51:11 +0200

    Add files via upload

--- send_mail.php
+++ send_mail.php
@@ -1,14 +1,22 @@
 <?php
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    $to = "redmanek@gmail.com"; // <-- tutaj wpisz swój adres e-mail
-    $subject = $_POST['subject'];
-    $message = "Od: ".$_POST['name']."\nEmail: ".$_POST['email']."\n\n".$_POST['message'];
-    $headers = "From: ".$_POST['email'];
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $name = htmlspecialchars($_POST['name']);
+    $email = htmlspecialchars($_POST['email']);
+    $subject = htmlspecialchars($_POST['subject']);
+    $message = htmlspecialchars($_POST['message']);
 
-    if(mail($to, $subject, $message, $headers)){
+    $to = "kontakt@wbtech.pl"; // Twój e-mail
+    $headers = "From: $email\r\n";
+    $body = "Imię i nazwisko: $name\nEmail: $email\nTemat: $subject\nWiadomość:\n$message";
+
+    if (mail($to, $subject, $body, $headers)) {
         echo "Wiadomość wysłana!";
     } else {
-        echo "Błąd przy wysyłaniu wiadomości.";
+        echo "Błąd podczas wysyłania wiadomości.";
     }
+} else {
+    // Jeśli nie POST
+    http_response_code(405);
+    echo "Metoda niedozwolona";
 }
 ?>
